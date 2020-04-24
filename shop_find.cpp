44f55e43403c1d2089a54aa23269d0407855883c
PeshinaA <62890415+PeshinaA@users.noreply.github.com>
2020-04-24 18:46:22 +0300

    Update shop_find.cpp

--- shop_find.cpp
+++ shop_find.cpp
@@ -1,22 +1,29 @@
 #include "shop_struct.h"
 
-void find(char name[80]){
+void find(int argc, char* argv[]){
 	int n;
-	char s[80];
+	string s;
 	int p;
 	int k = 0;
-	printf("[1]Find by type.\n[2]Find by color\n[3]Find by price.\n[4]Find by place.\n");
-	printf(">Enter the number: ");
+	cout<<"[1]Find by type."<<endl;
+	cout<<"[2]Find by color"<<endl;
+	cout<<"[3]Find by price."<<endl;
+	cout<<"[4]Find by place."<<endl;
+	cout<<" >Enter the number: "<<end;
 	cosmetic c1;
-	scanf("%d", &n);
-	FILE *f;
-	if ((f = fopen( name, "rb"))== NULL){
-		printf("  Cannot open file.\n");
-		exit(1);
+	cin>>n;
+	
+	ifstream f(argv[1]);
+	if (f.is_open())
+		cout<<"OK"<<endl;
+	else{
+		cout<<"Cannot open file."<<endl;
+		return 1;
 	}
+	
 	if (n == 1){
-		printf("\n>Enter type: ");
-		scanf("%s", s);
+		cout<<"\n>Enter type: "<<endl;
+		getline(cin, s);
 		while(!feof(f)){
 			fread(&c1, sizeof(c1), 1, f);
 			if (strcmp( s, c1.type)== 0) {
@@ -28,10 +35,10 @@ void find(char name[80]){
 			}
 		}
 		if (k == 0)
-			printf("  No products found.\n");
+			cout<<"  No products found."<<endl;
 	}else if (n == 2){
-		printf("\n>Enter color: ");
-		scanf("%s", s);
+		cout<<" >Enter color: "<<endl;
+		getline(cin, s);
 		while(!feof(f)){
 			fread(&c1, sizeof(c1), 1, f);
 			if (strcmp( s, c1.color)== 0) {
@@ -43,10 +50,10 @@ void find(char name[80]){
 			}
 		}
 		if (k == 0)
-			printf("  No products found.\n");
+			cout<<"  No products found."<<endl;
 	}else if (n == 3){
-		printf("\n>Enter price: ");
-		scanf("%d", &p);
+		cout<<" >Enter price: "<<endl;
+		cin>>p;
 		while(!feof(f)){
 			fread(&c1, sizeof(c1), 1, f);
 			if (c1.price == p) {
@@ -58,10 +65,10 @@ void find(char name[80]){
 			}
 		}
 		if (k == 0)
-			printf("  No products found.\n");
+			cout<<"  No products found."<<endl;
 	}else if (n == 4){
-		printf("\n>Enter place: ");
-		scanf("%d", &p);
+		cout<<" >Enter place: "<<endl;
+		cin>>p;
 		while(!feof(f)){
 			fread(&c1, sizeof(c1), 1, f);
 			if (c1.place == p) {
@@ -73,10 +80,10 @@ void find(char name[80]){
 			}
 		}
 		if (k == 0)
-			printf("  No products found.\n");
+			cout<<"  No products found."<<endl;
 	}else {
-		printf("  Please, enter a number from 1 to 4\n");
-		scanf("%d", &n);
+		cout<<"  Please, enter a number from 1 to 4."<<endl;
+		cin>>n;
 	}
-	fclose(f);
+	f.close();
 }
