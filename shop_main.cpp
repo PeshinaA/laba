a10fb2b609d8c332210f6a5fb59983e0582764fb
PeshinaA <62890415+PeshinaA@users.noreply.github.com>
2020-05-02 18:26:28 +0300

    Update shop_main.cpp

--- shop_main.cpp
+++ shop_main.cpp
@@ -6,18 +6,7 @@ int main(int argc, char* argv[]){
 	char *name = argv[1];
 	int size = 0;
 	int max = 100;
-/*	ifstream f(name);
-	if (!f.is_open()){
-		cout<<"Cannot open file."<<endl;
-		return 1;
-	}
-	
-	cosmetic c;
-	while(f.read((char *)&c, sizeof(cosmetic))){
-		size++;
-	}
-	f.close();
-*/
+
 	cosmetic * mass = new cosmetic[max];
 
 	menu(name, mass, size);
